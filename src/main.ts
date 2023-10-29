159f1304b09911ff9cd8028e5e66c354e70a6378
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2023-10-30 06:36:23 +0700

    feat(sentry): add sentry tracking error

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
-import { NestFactory, Reflector } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as Sentry from '@sentry/node';
+import { ProfilingIntegration } from '@sentry/profiling-node';
 import { AppModule } from './app.module';
 import { SuccessResponseInterceptor } from './utils/interceptors/success-response.interceptor';
+import { SentryFilter } from './utils/sentry-filter.utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  Sentry.init({
+    dsn: 'https://18b885a3cd1b64525e52dd787bd51903@o4506136204541952.ingest.sentry.io/4506136210178048',
+    integrations: [new ProfilingIntegration()],
+    // Performance Monitoring
+    tracesSampleRate: 1.0,
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
 
   app.useGlobalInterceptors(new SuccessResponseInterceptor());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
