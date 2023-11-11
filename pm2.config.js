9228bba123b5fe72b2aba2bb1a28234bede0087a
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2023-11-11 13:10:09 +0700

    Add npm rebuild for bcrypt in deployment script.

--- pm2.config.js
+++ pm2.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   name: 'online-classroom-pens-v1',
-  interpreter: '~/.bun/bin/bun',
   watch: true,
   script: './dist/src/main.js',
   ignore_watch: ['node_modules', 'dist', 'logs', 'public'],
