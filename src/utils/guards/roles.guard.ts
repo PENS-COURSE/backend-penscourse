a7197016f81ff6c8f2e0af67a014d8c090156380
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2024-03-19 15:49:16 +0700

    fix(roles): fix roles guard

--- src/utils/guards/roles.guard.ts
+++ src/utils/guards/roles.guard.ts
@@ -11,7 +11,7 @@ export class RolesGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const roles = this.reflector.get<Role[]>('roles', context.getHandler());
-    if (!roles) {
+    if (!roles || roles.length === 0) {
       return true;
     }
 
