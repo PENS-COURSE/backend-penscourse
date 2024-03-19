a4d61d34f4ac80579a45672b5b69aaf9de547802
Bintang Rezeka Ramadani <66295097+superrexy@users.noreply.github.com>
2024-03-19 16:28:22 +0700

    Revert "Revert "feat(quiz): add middleware""

--- src/courses/quiz/quiz.controller.ts
+++ src/courses/quiz/quiz.controller.ts
@@ -57,6 +57,7 @@ export class QuizController {
   @ApiOperation({
     summary: 'Get Quiz By Quiz',
   })
+  @UseGuards(HasEnrolledGuard)
   @ApiOkResponse()
   @HttpCode(200)
   @Get('quiz/:quiz_uuid')
@@ -128,6 +129,7 @@ export class QuizController {
 
   @UseGuards(HasEnrolledGuard)
   @Auth('user')
+  @UseGuards(HasEnrolledGuard)
   @ApiOperation({ summary: 'Submit Quiz' })
   @ApiOkResponse()
   @HttpCode(200)
@@ -149,6 +151,7 @@ export class QuizController {
 
   @UseGuards(HasEnrolledGuard)
   @Auth('user')
+  @UseGuards(HasEnrolledGuard)
   @ApiOperation({ summary: 'Get Result Quiz' })
   @ApiOkResponse()
   @Get('quiz/:quiz_uuid/result')
